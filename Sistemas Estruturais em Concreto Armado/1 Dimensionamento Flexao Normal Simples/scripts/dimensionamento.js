ebfb8c2f9a9d092d75dc95f4bf049369852662c5
Bruno Santos <bruno.if@hotmail.com>
2021-12-02 09:44:22 -0300

    Organizando as pastas

---
+++ Sistemas Estruturais em Concreto Armado/1 Dimensionamento Flexao Normal Simples/scripts/dimensionamento.js
@@ -0,0 +1,143 @@
+// Trecho JS do código
+
+function calcular() {
+  // Entrada de dados
+  var fck = document.getElementById("fck");
+  var fyk = document.getElementById("fyk");
+  var es = document.getElementById("es");
+  var gamac = document.getElementById("gamac");
+  var gamas = document.getElementById("gamas");
+  var gamaf = document.getElementById("gamaf");
+  var bduct = document.getElementById("bduct");
+  var b = document.getElementById("b");
+  var h = document.getElementById("h");
+  var d = document.getElementById("d");
+  var dl = document.getElementById("dl");
+  var amk = document.getElementById("amk");
+  var fck1 = Number(fck.value);
+  var fyk1 = Number(fyk.value);
+  var es1 = Number(es.value);
+  var gamac1 = Number(gamac.value);
+  var gamas1 = Number(gamas.value);
+  var gamaf1 = Number(gamaf.value);
+  var bduct1 = Number(bduct.value);
+  var b1 = Number(b.value);
+  h1 = Number(h.value);
+  var d1 = Number(d.value);
+  var dl1 = Number(dl.value);
+  var amk1 = Number(amk.value);
+  // Parâmetros do diagrama retangular
+  if (fck1 <= 50) {
+    alamb = 0.8;
+    alfac = 0.85;
+    eu = 3.5;
+    qlim = 0.8 * bduct1 - 0.35;
+  } else {
+    alamb = 0.8 - (fck1 - 50) / 400;
+
+    alfac = 0.85 * (1 - (fck1 - 50) / 200);
+    eu = 2.6 + 35 * ((90 - fck1) / 100) ** 4;
+    qlim = 0.8 * bduct1 - 0.45;
+  }
+  // Conversão de unidades: transformando para kN e cm
+  amk = 100 * amk1;
+  fck = fck1 / 10;
+  fyk = fyk1 / 10;
+  es = 100 * es1;
+  // Resistências de cálculo
+  fcd = fck / gamac1;
+  tcd = alfac * fcd;
+  fyd = fyk / gamas1;
+  amd = gamaf1 * amk;
+  // Parâmetro geométrico
+  delta = dl1 / d1;
+  // Momento limite
+  amilim = alamb * qlim * (1 - 0.5 * alamb * qlim);
+  // Momento reduzido solicitante
+  ami = amd / (b1 * d1 * d1 * tcd);
+  if (ami <= amilim) {
+    // Armadura simples
+    var qsi = (1 - Math.sqrt(1 - 2 * ami)) / alamb;
+    aas = (alamb * qsi * b1 * d1 * tcd) / fyd;
+    asl = 0;
+  } else {
+    // Armadura dupla
+    qsia = eu / (eu + 10);
+    if (qlim < qsia) {
+      // Evitando armadura dupla no domínio 2
+      var test = 1;
+      var aviso = "Aumentar a seção transversal!";
+    }
+    if (qlim <= delta) {
+      // Evitando armadura comprimida sob tração
+      var test = 1;
+      var aviso = "Aumentar a seção transversal!";
+    }
+    // Deformação da armadura de compressão
+    esl = (eu * (qlim - delta)) / qlim;
+    esl = esl / 1000;
+    ess = Math.abs(esl);
+    eyd = fyd / es;
+    // Cálculo da tensão no aço
+    if (ess < eyd) {
+      tsl = es * ess;
+    } else {
+      tsl = fyd;
+    }
+    if (esl < 0) {
+      tsl = -tsl;
+    }
+    // Cálculo das áreas de aço quando armadura dupla
+    asl = ((ami - amilim) * b1 * d1 * tcd) / ((1 - delta) * tsl);
+    aas = ((alamb * qlim + (ami - amilim) / (1 - delta)) * b1 * d1 * tcd) / fyd;
+  }
+  // Armadura mínima
+  a = 2 / 3;
+  fck = 10 * fck;
+  fyd = 10 * fyd;
+  if (fck <= 50) {
+    romin = (0.078 * fck ** a) / fyd;
+  } else {
+    romin = (0.5512 * Math.log10(1 + 0.11 * fck)) / fyd;
+  }
+  if (romin < 0.0015) {
+    romin = 0.0015;
+  }
+  asmin = romin * b1 * h1;
+  if (aas < asmin) {
+    aas = asmin;
+  }
+  // Verificação de erros e saída de resultados
+  while (
+    fck == 0 ||
+    fyk == 0 ||
+    es == 0 ||
+    gamac == 0 ||
+    gamas == 0 ||
+    gamaf == 0 ||
+    bduct == 0 ||
+    b == 0 ||
+    h == 0 ||
+    d == 0 ||
+    dl == 0 ||
+    amk == 0
+  ) {
+    test = 2;
+    window.alert("[ERRO] Problema na inserção dos dados de entrada!");
+    break;
+  }
+  if (test == 1) {
+    res.innerHTML = `<strong>${aviso}</strong>`;
+  } else if (test == 2) {
+    res.innerHTML = `<strong>Entre com os dados corretamente!</strong>`;
+  } else {
+    res.innerHTML = `<strong>A área de aço tracionada é ${aas.toFixed(
+      2
+    )} cm².</strong>`;
+    res1.innerHTML = `<strong>A área de aço comprimida é ${asl.toFixed(
+      2
+    )} cm².</strong>`;
+  }
+
+  redesenhar();
+}
---
+++ Sistemas Estruturais em Concreto Armado/1 Dimensionamento Flexao Normal Simples/scripts/redesenhar.js
@@ -0,0 +1,113 @@
+function redesenhar() {
+
+  fck = Number(document.getElementById("fck").value);
+  d = Number(document.getElementById("d").value);
+
+  //Cálculo do qsi
+  qsi = (1 - Math.sqrt(1 - 2 * ami)) / alamb;
+
+  //linha neutra
+  xa = qsi * d;
+
+  //Concreto do tipo 1
+  if (fck <= 50) {
+    eu = 3.5;
+  } else {
+    eu = 2.6 + 35 * ((90 - fck) / 100) ** 4;
+  }
+
+  //Verificar se a posição da linha neutra está acima do limite permitido pela NBR
+  if (fck <= 50) {
+    xlim = 0.45 * d;
+  } else {
+    xlim = 0.35 * d;
+  }
+
+  console.log("A posição da linha neutra é de " + xa + " cm");
+  console.log(
+    "É necessário comparar a linha neutra com o seu valor limite, o valor limite é de:" +
+      xlim +
+      " cm, nesse caso a posicao da linha neutra será:" +
+      Math.min(xa, xlim) +
+      " cm"
+  );
+
+  if (xa > xlim) {
+    dominio = "Domínio 3";
+    ruptura = " A ruptura acontece no concreto";
+    xa = xlim;
+    eps = (eu * (d - 1)) / xa;
+    epc = eu;
+  } else {
+    dominio = "Domínio 2";
+    ruptura = "A ruptura acontece no aço";
+    eps = 10 / 1000;
+    epc = (0.01 * xa) / (d - xa);
+  }
+
+  console.log("A peça se encontra no " + dominio);
+  console.log("A deformação no concreto é de " + epc);
+  console.log("A deformação no aço é de " + eps);
+
+  //Colocando os textos de deformação do aço e do concreto
+  criardiv2.innerText = (epc*1000).toFixed(3);
+  criardiv5.innerText = (eps*1000).toFixed(3);
+
+  //Linha neutra
+  ctx1.beginPath();
+  ctx1.strokeStyle = '#808080'
+  ctx1.lineWidth = "2";
+  ctx1.moveTo(150, 80 + ((230 * Math.min(xa, xlim)) / h1));
+  ctx1.lineTo(-150, 80 + ((230 * Math.min(xa, xlim)) / h1));
+  ctx1.stroke();
+
+  ctx2.beginPath();
+  ctx2.strokeStyle = '#808080'
+  ctx2.lineWidth = "2";
+  ctx2.moveTo(150, 80 + ((230 * Math.min(xa, xlim)) / h1));
+  ctx2.lineTo(-150, 80 + ((230 * Math.min(xa, xlim)) / h1));
+  ctx2.stroke();
+
+  ctx3.beginPath();
+  ctx3.strokeStyle = '#808080'
+  ctx3.lineWidth = "2";
+  ctx3.moveTo(150, 80 + ((230 * Math.min(xa, xlim)) / h1));
+  ctx3.lineTo(-150, 80 + ((230 * Math.min(xa, xlim)) / h1));
+  ctx3.stroke();
+
+
+  //Alamb
+  ctx1.beginPath();
+  ctx1.strokeStyle = '#ffa500'
+  ctx1.lineWidth = "2";
+  ctx1.moveTo(50, 80 + alamb*(230 * Math.min(xa, xlim)) / h1);
+  ctx1.lineTo(-50, 80 + alamb*(230 * Math.min(xa, xlim)) / h1);
+  ctx1.stroke();
+
+  //Escrevendo area de aço na figura 1
+
+  ctx1.beginPath();
+  ctx1.fillRect(-20,280,40,10);
+
+  ctx1.font="14px Montserrat";
+  ctx1.fillText(aas.toFixed(2) + ' cm²',-28,275);
+  
+
+  //Linha de deformação - Desenho 2 
+  ctx2.beginPath();
+  ctx2.strokeStyle = '#0000ff'
+  ctx2.lineWidth = "1";
+  ctx2.moveTo(-epc*14000,80);
+  ctx2.lineTo(eps*14000,287);
+  ctx2.stroke();
+
+  //escrevendo LN
+  ctx1.font="bold 20px Arial";
+  ctx1.fillStyle = '#808080'
+  ctx1.fillText(`L.N.`,-145, 75 + ((230 * Math.min(xa, xlim)) / h1));
+
+
+
+
+
+}
