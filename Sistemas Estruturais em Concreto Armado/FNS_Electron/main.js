4e36df523160d191ccf64253d66bc935a66b1b4a
Bruno Santos <bruno.if@hotmail.com>
2022-08-05 23:08:42 -0300

    Add gitignore

--- Sistemas Estruturais em Concreto Armado/FNS_Electron/main.js
+++ Sistemas Estruturais em Concreto Armado/FNS_Electron/main.js
@@ -1,14 +1,30 @@
-const {app} = require("electron")
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
-const createWindow = ()=>{
-    const win = new BrowserWindow({
-        width:1024,
-        height:720,
-        backgroundColor:'rgb(210, 115, 0)',
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-          }
-    }) 
-    win.loadFile('index.html')
-    win.webContents.openDevTools()
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
 }
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
